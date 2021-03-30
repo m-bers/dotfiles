dce32dccc4bdbd6af7ad265acf189bff328b660b
m-bers <jc4gavejc@gmail.com>
2021-03-29 18:15:25 -0700

    Update dot.sh

--- dot.sh
+++ dot.sh
@@ -7,9 +7,3 @@ dot config --local status.showUntrackedFiles no
 dot switch dot
 dot checkout $HOME
 dot submodule update --init --recursive
-cd ~/Library/Fonts && { 
-    curl -LO 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf'
-    curl -LO 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf'
-    curl -LO 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf'
-    curl -LO 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf'
-    cd -; }
